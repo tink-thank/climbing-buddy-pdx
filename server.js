c3ae22a47e822eb521f5adca4ed10eb13db94ebd
Jhenna Voorhis <jhenna.voorhis@gmail.com>
2014-04-08 15:50:02 -0700

    getting github auth

--- server.js
+++ server.js
@@ -1,17 +1,79 @@
 var express  = require('express'),
     passport = require('passport'),
-    path     = require('path');
+    path     = require('path'),
+    GitHubStrategy = require('passport-github').Strategy,
+    Q = require('q');
+var db = require('orchestrate')(process.env.ORCHESTRATE_API_KEY);
+
+// db.put('testUsers', 'test1', {name: 'Jhenna'});
+
+authUser = function (profile) {
+  var deferred = Q.defer();
+  console.log(profile);
+
+  // use the provider and the user (incase we support other providers down the road)
+  var key = 'jane1';
+
+  // the user data we want to store. this is mostly to clean up any extra data we don't need
+  var user = {
+    provider:    profile.provider,
+    id:          profile.id,
+    displayName: profile.displayName,
+    username:    profile.username,
+    profileUrl:  profile.profileUrl,
+    emails:      profile.emails,
+    avatar:      profile._json.avatar_url,
+    githubData:  profile._json
+  };
+
+  // put the user into our DB if it's not currently there
+  // this uses Orchestrate's condition PUT functionality
+  db.put('testUsers', key, user)
+  .then(function () {
+    // add the key to the session data
+    user.key = key;
+    deferred.resolve(user);
+  })
+  .fail(function () {
+    deferred.reject();
+  });
+
+  return deferred.promise;
+}
 
 var app      = express();
 
 var port     = process.env.PORT || 5000;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GitHubStrategy(
+    {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.GITHUB_CALLBACK
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // on successful auth
+        authUser(profile)
+        .then(function (user) {
+          done(null, user);
+        });
+    }
+));
 
 // configure Express
 app.use(express.logger());
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.session({ secret: 'cookie monster' }));
+app.use(express.session({ secret: 'climbing master' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
@@ -19,6 +81,13 @@ app.use(express.static(path.join(__dirname, 'public_html')));
 
 app.get('/', function(req, res){
     res.sendfile('./public_html/index.html');
+});
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
 });
 
 app.listen(port);

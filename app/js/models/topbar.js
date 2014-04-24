599b7f7703db54696de23d1dbb89829be5afd4aa
trippel <cpeddecord@gmail.com>
2014-04-24 08:42:42 -0700

    altered model to test

--- app/js/models/topbar.js
+++ app/js/models/topbar.js
@@ -1,9 +1,13 @@
 var TopBar = Thorax.Model.extend({
   urlRoot: '/user',
+//  defaults: {
+//    userName: 'Testing UserName, Please Ignore',
+//    userImg: 'http://www.placekitten.com/75/75'
+//  }
   defaults: {
-    userName: 'Testing UserName, Please Ignore',
-    userImg: 'http://www.placekitten.com/75/75'
+    displayName: 'Test User',
+    avatar: 'http://www.placekitten.com/75/75'
   }
-  });
+});
 
 module.exports = TopBar;

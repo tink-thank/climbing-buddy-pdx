8ab81c5e0a5703a7ea5a73e0237f25e8ef3ee112
trippel <cpeddecord@gmail.com>
2014-04-26 14:02:41 -0700

    stupid posting, no more default

--- app/js/models/posting.js
+++ app/js/models/posting.js
@@ -7,18 +7,18 @@ var Posting = Thorax.Model.extend({
     this.save();
   },
   
-  defaults: {
-    postingId: 10000,
-    title: 'default post, please ignore',
-    timeStamp: Date.now(),
-    userName: 'Alex Honnold Default',
-    userImg: 'http://lorempixel.com/75/75',
-    climbGym: 'Circuit NE',
-    climbEta: '30',
-    climbDuration: '60',
-    climbDetails: 'Sending V-10\'s like what',
-    replies: []
-  }
+//  defaults: {
+//    postingId: 10000,
+//    title: 'default post, please ignore',
+//    timeStamp: Date.now(),
+//    userName: 'Alex Honnold Default',
+//    userImg: 'http://lorempixel.com/75/75',
+//    climbGym: 'Circuit NE',
+//    climbEta: '30',
+//    climbDuration: '60',
+//    climbDetails: 'Sending V-10\'s like what',
+//    replies: []
+//  }
 
 });
 

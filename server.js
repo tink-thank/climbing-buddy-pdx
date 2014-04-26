4f3a628c8dfcc2e03996dc2cec11476c2ccf51f3
trippel <cpeddecord@gmail.com>
2014-04-26 16:19:07 -0700

    fix server before merge, yo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express  = require('express'),
 var db = require('orchestrate')(process.env.ORCHESTRATE_API_KEY);
 
 var app      = express();
-var port     = 9000;
+var port     = 80;
 
 // ===========================
 // PASSPORT
@@ -77,7 +77,7 @@ app.use(function(req, res, next) {
 // ===========================
 
 // Homepage
-app.get('/', function (req, res){
+app.get('/', isLoggedIn, function (req, res){
     res.sendfile('./public_html/index.html');
 });
 
@@ -96,7 +96,7 @@ app.get('/auth/github/callback', passport.authenticate('github', { failureRedire
 
 // route to send user information to the front end
 app.get('/user', function (req, res){
-  res.json(200, {"displayName":"CHRIS SHARMA"});
+  res.json(200, req.user);
 })
 
 //logout
@@ -109,7 +109,7 @@ app.get('/logout', function(req, res){
 })
 
 // get posts from database
-app.get('/posts', function(req, res){
+app.get('/posts', isLoggedIn, function(req, res){
   console.log("GETTING /POSTS");
   var postsList = [];
   
@@ -140,7 +140,7 @@ app.get('/posts', function(req, res){
   
 });
 
-// app.get('posts/*', function (req, res){
+// app.get('posts/*', isLoggedIn, function (req, res){
 //   db.get('testPosts', 'post' + req.body.id)
 //   .then(function (result){
 //     res.json(200, result.body.results);
@@ -150,7 +150,7 @@ app.get('/posts', function(req, res){
 //   })
 // });
 
-app.post('/posts/*', function (req, res) {
+app.post('/posts/*', isLoggedIn, function (req, res) {
   // console.log(req.body);
   // console.log(req.body.id); //need to fix so only getting back the part after the colon (right now it is included)
   db.put('testPosts', 'post' + req.body.id, req.body)
@@ -163,7 +163,7 @@ app.post('/posts/*', function (req, res) {
   });
 });
 
-app.put('/posts/*', function (req, res) {
+app.put('/posts/*', isLoggedIn, function (req, res) {
   // console.log(req.body);
   // console.log(req.body.id); //need to fix so only getting back the part after the colon (right now it is included)
   db.put('testPosts', 'post' + req.body.id, req.body)
@@ -176,7 +176,7 @@ app.put('/posts/*', function (req, res) {
   });
 });
 
-app.delete('/posts/*',  function (req, res){
+app.delete('/posts/*',isLoggedIn,  function (req, res){
   db.remove('testPosts', post + req.body.id, true)
   .then(function (result) {
     console.log("SUCCESSFULLY REMOVED FROM DB");

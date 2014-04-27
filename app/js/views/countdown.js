0f97ce48d9bf1f92250dfd46825d6b1114199975
dan125 <dberezhinskiy@gmail.com>
2014-04-27 15:43:23 -0700

    added hours to countdown timer

--- app/js/views/countdown.js
+++ app/js/views/countdown.js
@@ -11,6 +11,9 @@ setInterval(function () {
  
     var current_date = new Date().getTime();
     var seconds_left = (target_date - current_date) / 1000;
+
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
      
     minutes = parseInt(seconds_left / 60);
     seconds = parseInt(seconds_left % 60);

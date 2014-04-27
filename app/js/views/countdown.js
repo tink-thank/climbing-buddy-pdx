0bb31dedcf1f6c999fe44dc9ec4313bb4a02caef
dan125 <dberezhinskiy@gmail.com>
2014-04-27 15:07:25 -0700

    countdown timer

---
+++ app/js/views/countdown.js
@@ -0,0 +1,19 @@
+
+var target_date = new Date().getTime();
+ 
+var minutes;
+var seconds;
+ 
+var countdown = document.getElementById("countdown");
+ 
+setInterval(function () {
+ 
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    countdown.innerHTML = minutes + " : " + seconds;  
+ 
+}, 1000);
